682ad8a0b40d0695dcb2a462202647e64d0f4877
Aysenur <aysenurtak1@gmail.com>
2025-05-04 01:05:45 +0300

    models.py is created

---
+++ backend/utils/models.py
@@ -0,0 +1,20 @@
+"""
+User
+- id: int
+- username: str
+- email: str
+- password: str
+
+
+Project
+- id: int
+- name: str
+- image: str
+- description: str
+- user_id: int
+- created_at: datetime
+- updated_at: datetime
+- done: bool
+- is_public: bool
+
+"""
